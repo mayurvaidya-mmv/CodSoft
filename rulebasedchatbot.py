58004b6e431e71de3e62a84ce65a66248a2e0ff5
mayurvaidya-mmv <116434512+mayurvaidya-mmv@users.noreply.github.com>
2023-11-13 14:13:38 +0530

    rulebasedchatbot.py

--- rulebasedchatbot.py
+++ rulebasedchatbot.py
@@ -1,79 +1,50 @@
 import random
 
 class RuleBot:
+    negative_res = ("no", "nope", "sorry")
+    exit_commands = ("quit", "exit", "bye")
+    random_questions = [
+        "Why are you here?",
+        "Are there many humans like you?",
+        "Is there intelligent life on this planet?",
+        "Does Earth have a leader?"
+    ]
+
     def __init__(self):
-        self.exit_commands = ("quit", "exit", "bye")
+        self.alien_babble = {
+            'describe_planet_intent': 'your planet',
+            'answer_why_intent': 'why are',
+            'about_codsoft': 'codsoft'
+        }
 
     def greet(self):
         name = input("Your name?\n")
-        will_help = input(f"Hello {name}, I'm a bot. Can you help me learn about your planet?\n")
-        if will_help.lower() in ("no", "nope", "sorry"):
+        will_help = input(f"Hi {name}, I'm a bot. Can you help me learn about your planet?\n")
+        if will_help.lower() in self.negative_res:
             print("Enjoy your day on Earth!")
-        else:
-            self.chat()
+            return
+        self.chat()
+
+    def make_exit(self, reply):
+        return reply in self.exit_commands
 
     def chat(self):
         while True:
-            user_input = input(self.get_random_question()).lower()
-            if self.make_exit(user_input):
-                print("Have a good day!")
+            reply = input(random.choice(self.random_questions) + "\n").lower()
+            if self.make_exit(reply):
+                print("Goodbye!")
                 break
-            else:
-                self.respond(user_input)
+            self.respond(reply)
 
-    def get_random_question(self):
-        questions = [
-            "What's the reason behind your visit?",
-            "Are there others like you?",
-            "What do you eat here?",
-            "Is there intelligent life here?",
-            "Is there a leader here?"
-        ]
-        return random.choice(questions) + "\n"
-
-    def make_exit(self, user_input):
-        return user_input in self.exit_commands
-
-    def respond(self, user_input):
-        if "your planet" in user_input:
-            print(self.describe_planet_intent())
-        elif "why are" in user_input:
-            print(self.answer_why_intent())
-        elif "codehub" in user_input:
-            print(self.about_codehub())
+    def respond(self, reply):
+        if 'your planet' in reply:
+            print(random.choice(["My planet is diverse.", "Food here is great."]))
+        elif 'why are' in reply:
+            print(random.choice(["I come in peace.", "I'm here to learn.", "Food is good here."]))
+        elif 'codsoft' in reply:
+            print("CodSoft is an innovative educational platform providing diverse learning opportunities and fostering skill development.")
         else:
-            print(self.no_match_intent())
-
-    def describe_planet_intent(self):
-        responses = [
-            "My planet is full of different beings.",
-            "I've heard the food here is great."
-        ]
-        return random.choice(responses)
-
-    def answer_why_intent(self):
-        responses = [
-            "I come in peace.",
-            "I'm here to understand your planet and its inhabitants.",
-            "I've heard the food here is good."
-        ]
-        return random.choice(responses)
-
-    def about_codehub(self):
-        responses = [
-            "Codehub is an educational platform.",
-            "Codehub helps in learning new things.",
-            "Codehub is a place for growth."
-        ]
-        return random.choice(responses)
-
-    def no_match_intent(self):
-        responses = [
-            "Could you tell me more?",
-            "Please share more.",
-            "Why?"
-        ]
-        return random.choice(responses)
+            print(random.choice(["Please tell me more.", "Can you elaborate?", "Why?"]))
 
 bot = RuleBot()
 bot.greet()
